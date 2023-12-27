4e8811fe286e94a7b8e7b9498fe9f00af0c52860
Piotr Plywacz <piotrus.plywacz@gmail.com>
2023-12-27 23:39:11 +0100

    running programmes with parameters

--- rozw/pp448454/mimpirun.c
+++ rozw/pp448454/mimpirun.c
@@ -1,9 +1,37 @@
-/**
- * This file is for implementation of mimpirun program.
- * */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "mimpi_common.h"
 
-int main() {
-    TODO
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Wrong number of arguments\n");
+        return 1;
+    }
+    int n = atoi(argv[1]);
+    char* path = argv[2];
+
+    for (int i = 0; i < n; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            char* args[argc - 1];
+            args[0] = path;
+            for (int j = 3; j < argc; j++) {
+                args[j - 2] = argv[j];
+            }
+            args[argc - 2] = NULL;
+            execv(path, args);  // TODO : czy to jest ok - cos mialo sie znajdowac w PATH?
+            printf("Failed to execute program\n");
+            return 1;
+        }
+    }
+    // Wait for all child processes to finish.
+    for (int i = 0; i < n; i++) {
+        ASSERT_SYS_OK(wait(NULL));
+    }
+
+    return 0;
 }
---
+++ rozw/pp448454/test.c
@@ -0,0 +1,65 @@
+#include "stdio.h"
+
+int main(int argc, char **argv) {
+    sleep(1);
+    printf("arc: %d\n", argc);
+    for(int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
+// #include <unistd.h>
+
+// const int rozmiar = 1000000;
+// void dziecko_pisze() {
+//     return;
+//     char wiadomosc1[rozmiar];
+    
+//     for(int i = 0; i < rozmiar; i++)
+//         wiadomosc1[i] = 'a';
+
+//     int a =  write(20, wiadomosc1, sizeof(wiadomosc1) - 1);
+//     //sleep(1);
+//     printf("a: %d\n", a + 1);
+// }
+
+
+// void rodzic_czyta() {
+//     int ile = 0;
+//     char* xd = getenv("xd");
+//     printf("xd: %s\n", xd);
+//     return;
+//     while(ile < rozmiar - 1) {
+//         char wiadomosc2[rozmiar];    
+//         int b = read(20, wiadomosc2, sizeof(wiadomosc2));
+//         printf("b: %d\n", b);
+//         ile += b;
+//     }
+// }
+
+// int main() {
+//     setenv("xd", "1000", 1);
+//     int pipe_dsc[2];
+//     pipe(pipe_dsc);
+//     pid_t pid = fork();
+//     if(pid == 0) {
+//         // child
+//         close(pipe_dsc[0]);
+//         dup2(pipe_dsc[1], 20); // moje deskryptory to od 20 do 1023 wlacznie
+//         dziecko_pisze();
+//         close(pipe_dsc[1]);
+//     }
+//     else {
+//         // parent
+//         close(pipe_dsc[1]);
+//         dup2(pipe_dsc[0], 20);
+//         rodzic_czyta();
+//         close(pipe_dsc[0]);
+//         wait(NULL);
+//     }
+
+// }
